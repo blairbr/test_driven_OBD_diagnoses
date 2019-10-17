bad4849cad373207a9acc301e397e81ac87ab262
blair.brown <blair.brown@accenture.com>
2019-10-17 17:41:29 -0400

    removed constructor in test file so tests run

--- test_car_doctor.py
+++ test_car_doctor.py
@@ -5,23 +5,18 @@ from CarDoctor import CarDoctor
 
 class Test_Car_Doctor(unittest.TestCase):
 
-    def __init__(self):
-        self.doc = CarDoctor()
-
     def test_get_gas_cap_suggestion_for_441(self):
         expected_suggestion = 'Try tightening your gas cap'
-        actual_suggestion = self.doc.append_troubleshooting_suggestion(self)
+        # here need to mock out the call and tell it to get a specific code back
 
-        self.assertEqual(expected_suggestion, actual_suggestion)
-
-    def test_get_gas_cap_suggestion_for_441(self):
-        expected_suggestion = 'Try tightening your gas cap'
-        actual_suggestion = self.doc.append_troubleshooting_suggestion(self)
+        actual_suggestion = CarDoctor.append_troubleshooting_suggestion(self)
 
         self.assertEqual(expected_suggestion, actual_suggestion)
 
 
     def test_get_see_mechanic_suggestion_for_300to305(self):
         expected_suggestion = 'Your engine is misfiring. One or more of the cylinders is not working properly. See a mechanic.'
-        actual_suggestion = self.doc.append_troubleshooting_suggestion(self);
+
+        actual_suggestion = CarDoctor.append_troubleshooting_suggestion(self);
+
         self.assertEqual(expected_suggestion, actual_suggestion)
