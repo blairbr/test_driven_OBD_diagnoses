a9bc64507c783537ae5e80796c7011f67671b489
blair.brown <blair.brown@accenture.com>
2019-10-18 16:07:35 -0400

    fix tests

--- CarDoctor.py
+++ CarDoctor.py
@@ -5,25 +5,12 @@ import requests
 
 class CarDoctor:
     # obd.commands.FREEZE_DTC
-        error_codes = {
-            "P0104":  {
-                "definition": "Mass or Volume Air Flow Circuit Intermittent",
-                "suggestion": "Check for frayed wires or loose connections to your MAF sensor."
-            },
-            "P0441":  {
-                "definition": "Evaporative Emission Control System Incorrect Purge Flow",
-                "suggestion": "Try tightening your gas cap."
-            },
-            "P0300": {
-                "definition": "One or more of the cylinders is not working properly. See a mechanic.",
-                "suggestion": "Try tightening your gas cap."
-            }
-        }
 
 
-        error_104 = ("P0104", "Mass or Volume Air Flow Circuit Intermittent")
-        error_441 = ("P0441", "Try tightening your gas cap.")  # was this an array??
-        error_300 = ("P0300", "Random/Multiple cylinder misfire detected.")
+
+        error_104 = [("P0104", "Mass or Volume Air Flow Circuit Intermittent")]
+        error_441 = [("P0441", "Try tightening your gas cap.")]  # was this an array??
+        error_300 = [("P0300", "Random/Multiple cylinder misfire detected.")]
         def get_error_codes(self):
             connection = obd.OBD()
             dtc_codes = connection.query(obd.commands.GET_DTC)
@@ -38,19 +25,31 @@ class CarDoctor:
             return current_DT
 
         def append_troubleshooting_suggestion(self, errorcode):
-            if errorcode == ("P0300", "Random/Multiple cylinder misfire detected."):
-                return 'One or more of the cylinders is not working properly. See a mechanic.'
-            elif errorcode == ("P0441", "Evaporative Emission Control System Incorrect Purge Flow"):
-                return 'Try tightening your gas cap'
-            elif errorcode == ("P0104", "Mass or Volume Air Flow Circuit Intermittent"):
-                return 'Check for frayed wires or loose connections to your MAF sensor.'
-            else:
-                return 'Unrecognized code. No suggestions for you yet'
+
+            error_codes = {
+                "P0104": {
+                    "definition": "Mass or Volume Air Flow Circuit Intermittent",
+                    "suggestion": "Check for frayed wires or loose connections to your MAF sensor."
+                },
+                "P0441": {
+                    "definition": "Evaporative Emission Control System Incorrect Purge Flow",
+                    "suggestion": "Try tightening your gas cap."
+                },
+                "P0300": {
+                    "definition": "One or more of the cylinders is not working properly. See a mechanic.",
+                    "suggestion": "Your engine is misfiring. One or more of the cylinders is not working properly. See a mechanic."
+                }
+            }
+            return error_codes.get(errorcode[0][0]).get('suggestion')
 
 
         def log_information(self):
             logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
             logging.debug(self.dtc_codes)
+
+
+            #Future steps:
+            #make the error codes dictionary a global variable
 
 
 
--- test_car_doctor.py
+++ test_car_doctor.py
@@ -6,13 +6,13 @@ from CarDoctor import CarDoctor
 class Test_Car_Doctor(unittest.TestCase):
     error_104 = ("P0104", "Mass or Volume Air Flow Circuit Intermittent")
 
-    def test_get_gas_cap_suggestion_for_441(self):
+    def test_get_check_wires_suggestion_for_104(self):
         expected_suggestion = 'Check for frayed wires or loose connections to your MAF sensor.'
 
         # here need to mock out the call and tell it to get a specific code back??
 
         mock = Mock()
-        mock.CarDoctor.get_error_codes.return_value = ("P0104", "Mass or Volume Air Flow Circuit Intermittent")
+        mock.CarDoctor.get_error_codes.return_value = [("P0104", "Mass or Volume Air Flow Circuit Intermittent")]
         mocked_error_code = mock.CarDoctor.get_error_codes()
 
         actual_suggestion = CarDoctor.append_troubleshooting_suggestion(self, mocked_error_code)
@@ -22,7 +22,7 @@ class Test_Car_Doctor(unittest.TestCase):
     def test_get_see_mechanic_suggestion_for_300to305(self):
         expected_suggestion = 'Your engine is misfiring. One or more of the cylinders is not working properly. See a mechanic.'
 
-        error_300 = ("P0300", "Random/Multiple cylinder misfire detected.")
+        error_300 = [("P0300", "Random/Multiple cylinder misfire detected.")]
 
         mock = Mock()
         mock.CarDoctor.get_error_codes.return_value = error_300
@@ -32,9 +32,9 @@ class Test_Car_Doctor(unittest.TestCase):
         self.assertEqual(expected_suggestion, actual_suggestion)
 
     def test_get_gas_cap_suggestion_for_441(self):
-        expected_suggestion = 'Try tightening your gas cap'
+        expected_suggestion = 'Try tightening your gas cap.'
 
-        error_441 = ("P0441", "Evaporative Emission Control System Incorrect Purge Flow")
+        error_441 = [("P0441", "Evaporative Emission Control System Incorrect Purge Flow")]
 
         mock = Mock()
         mock.CarDoctor.get_error_codes.return_value = error_441
