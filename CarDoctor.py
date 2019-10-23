9752499178ad3cfce0966d6406d319fae4d4beb9
unknown <blair.j.brown@gmail.com>
2019-10-23 19:55:36 -0400

    Added a setup method in test_car_doctor.py

--- CarDoctor.py
+++ CarDoctor.py
@@ -4,66 +4,38 @@ import logging
 import yaml
 
 class CarDoctor:
-    # obd.commands.FREEZE_DTC
 
-        error_104 = [("P0104", "Mass or Volume Air Flow Circuit Intermittent")]
-        error_441 = [("P0441", "Try tightening your gas cap.")]
-        error_300 = [("P0300", "Random/Multiple cylinder misfire detected.")]
+    def get_error_codes(self):
+        connection = obd.OBD()
+        dtc_codes = connection.query(obd.commands.GET_DTC)
+        if dtc_codes:
+            for dtc_code in dtc_codes:
+                return dtc_code
+        else:
+            return 'no error codes found'
 
-        def get_error_codes(self):
-            connection = obd.OBD()
-            dtc_codes = connection.query(obd.commands.GET_DTC)
-            if dtc_codes:
-                for dtc_code in dtc_codes:
-                    return dtc_code
-            else:
-                return 'no error codes found'
+    def trim_dtc_code_to_four_digit_error_code(self, dtc):
+        return dtc[0][0]
 
-        def trim_dtc_code_to_four_digit_error_code(self, dtc):
-            return dtc[0][0]
+    def print_timestamp(self):
+        current_DT = datetime.datetime.now()
+        return current_DT
 
-        def print_timestamp(self):
-            current_DT = datetime.datetime.now()
-            return current_DT
+    # note to self - fix this to the relative path so it'll work on other humans' machines
+    def retrieve_troubleshooting_suggestion(self, error_code):
+        with open(r'C:\Users\Blair\Projects\PythonProjects\CarDoctorFolder\test_driven_OBD_diagnoses\venv\error_codes_yaml.yaml') as yaml_file:
+            doc = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
-        def retrieve_troubleshooting_suggestion(self, error_code):
-           # return error_codes.get(errorcode[0][0]).get('suggestion')
-            # read from the yaml and get the suggestion here.
-            with open(r'C:\Users\Blair\Projects\PythonProjects\CarDoctorFolder\test_driven_OBD_diagnoses\venv\error_codes_yaml.yaml') as yaml_file:
-                doc = yaml.load(yaml_file, Loader=yaml.FullLoader)
+        suggestion = doc[error_code]["suggestion"]
+        return suggestion
 
-            suggestion = doc[error_code]["suggestion"]
-            return(suggestion)
+    def log_information(self):
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+        logging.debug(self.dtc_codes)
 
-        def log_information(self):
-            logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-            logging.debug(self.dtc_codes)
+    # note to self - fix this to the relative path so it'll work on other humans' machines
+    def retrieve_yaml_file(self):
+        with open(r'C:\Users\Blair\Projects\PythonProjects\CarDoctorFolder\test_driven_OBD_diagnoses\venv\error_codes_yaml.yaml') as file:
+            error_codes_list = yaml.load(file, Loader=yaml.FullLoader)
 
-        def retrieve_yaml_file(self):
-            with open(r'C:\Users\Blair\Projects\PythonProjects\CarDoctorFolder\test_driven_OBD_diagnoses\venv\error_codes_yaml.yaml') as file:
-                # The FullLoader parameter handles the conversion from YAML
-                # scalar values to Python the dictionary format
-                error_codes_list = yaml.load(file, Loader=yaml.FullLoader)
-
-                return error_codes_list
-            #Future steps:
-            #make the error codes dictionary a global variable
-            #move it into a yaml file
-
-
-
-
-            #error_codes = {
-                # "P0104": {
-                #     "definition": "Mass or Volume Air Flow Circuit Intermittent",
-                #     "suggestion": "Check for frayed wires or loose connections to your MAF sensor."
-                # },
-                # "P0441": {
-                #     "definition": "Evaporative Emission Control System Incorrect Purge Flow",
-                #     "suggestion": "Try tightening your gas cap."
-                # },
-                # "P0300": {
-                #     "definition": "One or more of the cylinders is not working properly. See a mechanic.",
-                #     "suggestion": "Your engine is misfiring. One or more of the cylinders is not working properly. See a mechanic."
-                # }
-            # }
+            return error_codes_list
--- test_car_doctor.py
+++ test_car_doctor.py
@@ -1,49 +1,46 @@
 import unittest
-
 from unittest.mock import Mock
 from CarDoctor import CarDoctor
 
-
-#TO DO- find a way to strip the [( or whatever off the incoming error codes so they can be passed along as just a string
-#refactor the code so that all three error code tests are passing
+#To-do
 #refactor in general and figure out how to make global variables, add in __init__ constructor
 #   https://stackoverflow.com/questions/37626662/get-yaml-key-value-in-python
+#clean up the tests so that they are fixtures that just pass in different stuff instead of all different tests
 
 class Test_Car_Doctor(unittest.TestCase):
-    error_104 = ("P0104", "Mass or Volume Air Flow Circuit Intermittent")
-    mock = Mock()
+
+    def setUp(self):
+        self.mock = Mock()
 
     def test_get_check_wires_suggestion_for_104(self):
         expected_suggestion = 'Check for frayed wires or loose connections to your MAF sensor.'
 
-        mock = Mock()
-        mock.CarDoctor.get_error_codes.return_value = 'P0104' #[("P0104", "Mass or Volume Air Flow Circuit Intermittent")]
-        mocked_error_code = mock.CarDoctor.get_error_codes()
-
-        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, mocked_error_code)
+        self.mock.CarDoctor.get_error_codes.return_value = [("P0104", "Mass or Volume Air Flow Circuit Intermittent")]
+        mocked_dtc = self.mock.CarDoctor.get_error_codes()
+        trimmed_error_code = CarDoctor.trim_dtc_code_to_four_digit_error_code(self, mocked_dtc)
+        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, trimmed_error_code)
         self.assertEqual(expected_suggestion, actual_suggestion)
 
-    def test_get_see_mechanic_suggestion_for_300to305(self):
+    def test_get_see_mechanic_suggestion_for_300(self):
         expected_suggestion = 'Your engine is misfiring. One or more of the cylinders is not working properly. See a mechanic.'
 
-        error_300 = 'P0300' # [("P0300", "Random/Multiple cylinder misfire detected.")]
-
-        mock = Mock()
-        mock.CarDoctor.get_error_codes.return_value = error_300
-        mocked_error_code = mock.CarDoctor.get_error_codes()
-        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, mocked_error_code)
+        error_300 = [("P0300", "Random/Multiple cylinder misfire detected.")]
+        self.mock.CarDoctor.get_error_codes.return_value = error_300
+        mocked_dtc = self.mock.CarDoctor.get_error_codes()
+        trimmed_error_code = CarDoctor.trim_dtc_code_to_four_digit_error_code(self, mocked_dtc)
+        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, trimmed_error_code)
 
         self.assertEqual(expected_suggestion, actual_suggestion)
 
     def test_get_gas_cap_suggestion_for_441(self):
         expected_suggestion = 'Try tightening your gas cap.'
 
-        error_441 = 'P0441'  # [("P0441", "Evaporative Emission Control System Incorrect Purge Flow")]
+        error_441 = [("P0441", "Evaporative Emission Control System Incorrect Purge Flow")]
 
-        mock = Mock()
-        mock.CarDoctor.get_error_codes.return_value = error_441
-        mocked_error_code = mock.CarDoctor.get_error_codes()
-        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, mocked_error_code)
+        self.mock.CarDoctor.get_error_codes.return_value = error_441
+        mocked_dtc = self.mock.CarDoctor.get_error_codes()
+        trimmed_error_code = CarDoctor.trim_dtc_code_to_four_digit_error_code(self, mocked_dtc)
+        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, trimmed_error_code)
 
         self.assertEqual(expected_suggestion, actual_suggestion)
 
