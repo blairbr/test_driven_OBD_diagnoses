d6a820369216c5f97bdd4b2d4ff5165b2ea92aac
blair.brown <blair.brown@accenture.com>
2019-11-11 08:24:23 -0500

    Added OBD2_codes.py

--- test_car_doctor.py
+++ test_car_doctor.py
@@ -12,12 +12,20 @@ class Test_Car_Doctor(unittest.TestCase):
     def setUp(self):
         self.mock = Mock()
 
+    def test_error_code_is_trimmed_correctly(self):
+        expected = 'P0104'
+        dtc = [("P0104", "Mass or Volume Air Flow Circuit Intermittent")]
+        actual = CarDoctor.trim_dtc_code_to_four_digit_error_code(self, dtc)
+        self.assertEqual(expected, actual)
+
+
     def test_get_check_wires_suggestion_for_104(self):
         expected_suggestion = 'Check for frayed wires or loose connections to your MAF sensor.'
 
-        self.mock.CarDoctor.get_error_codes.return_value = [("P0104", "Mass or Volume Air Flow Circuit Intermittent")]
-        mocked_dtc = self.mock.CarDoctor.get_error_codes()
-        trimmed_error_code = CarDoctor.trim_dtc_code_to_four_digit_error_code(self, mocked_dtc)
+        # self.mock.CarDoctor.get_error_codes.return_value = [("P0104", "Mass or Volume Air Flow Circuit Intermittent")]
+        # mocked_dtc = self.mock.CarDoctor.get_error_codes()
+
+        trimmed_error_code = 'P0104'
         actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, trimmed_error_code)
         self.assertEqual(expected_suggestion, actual_suggestion)
 
@@ -25,10 +33,7 @@ class Test_Car_Doctor(unittest.TestCase):
         expected_suggestion = 'Your engine is misfiring. One or more of the cylinders is not working properly. See a mechanic.'
 
         error_300 = [("P0300", "Random/Multiple cylinder misfire detected.")]
-        self.mock.CarDoctor.get_error_codes.return_value = error_300
-        mocked_dtc = self.mock.CarDoctor.get_error_codes()
-        trimmed_error_code = CarDoctor.trim_dtc_code_to_four_digit_error_code(self, mocked_dtc)
-        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, trimmed_error_code)
+        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, error_300)
 
         self.assertEqual(expected_suggestion, actual_suggestion)
 
@@ -36,11 +41,7 @@ class Test_Car_Doctor(unittest.TestCase):
         expected_suggestion = 'Try tightening your gas cap.'
 
         error_441 = [("P0441", "Evaporative Emission Control System Incorrect Purge Flow")]
-
-        self.mock.CarDoctor.get_error_codes.return_value = error_441
-        mocked_dtc = self.mock.CarDoctor.get_error_codes()
-        trimmed_error_code = CarDoctor.trim_dtc_code_to_four_digit_error_code(self, mocked_dtc)
-        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, trimmed_error_code)
+        actual_suggestion = CarDoctor.retrieve_troubleshooting_suggestion(self, error_441)
 
         self.assertEqual(expected_suggestion, actual_suggestion)
 
